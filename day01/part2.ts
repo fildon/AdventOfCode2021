056d94a022cf555a847218ad5187289eefc26181
Rupert Foggo McKay <rupert.mckay@adidas.com>
2021-12-01 11:16:33 +0100

    day01/part2 ✅ SOLVED

---
+++ day01/part2.ts
@@ -0,0 +1,25 @@
+import { input } from "./part2Input.ts";
+
+/**
+ * Task: Your goal now is to count the number of times the sum of
+ * measurements in this sliding window increases from the previous
+ * sum.
+ *
+ * Solution approach:
+ * When comparing adjacent rolling windows, shared values can be
+ * ignored. Only the first value in the prior window and the last
+ * value in the current window matter. So we can compare these values
+ * directly.
+ */
+const countWindowIncreases = (depthMeasurements: number[]): number => {
+  let increases = 0;
+  // Skip first two entries
+  for (let i = 3; i < depthMeasurements.length; i++) {
+    if (depthMeasurements[i] > depthMeasurements[i - 3]) increases++;
+  }
+  return increases;
+};
+
+const solution = countWindowIncreases(input);
+
+console.log(solution); // 1346
