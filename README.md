5f5c0d0ea9a94a7b3ea584b127bca54133c15d16
Rupert Foggo McKay <rupert.mckay@adidas.com>
2021-12-01 11:04:54 +0100

    day01/part1 ✅ SOLVED

---
+++ README.md
@@ -0,0 +1,13 @@
+# Advent of Code 2021
+
+This repository contains my solutions to the puzzles in [Advent of Code 2021](https://adventofcode.com/2021)
+
+## Prerequisites
+
+- [Deno](https://deno.land/)
+
+## Executing solutions
+
+```shell
+deno run day01/part1.ts
+```
---
+++ day01/part1.ts
@@ -0,0 +1,18 @@
+import { input } from "./part1Input.ts";
+
+/**
+ * count the number of times a depth measurement increases from the previous measurement.
+ * (There is no measurement before the first measurement.)
+ */
+const countDepthIncreases = (depthMeasurements: number[]): number => {
+  let increases = 0;
+  // Start at 1 to skip first measurement
+  for (let i = 1; i < depthMeasurements.length; i++) {
+    if (depthMeasurements[i] > depthMeasurements[i - 1]) increases++;
+  }
+  return increases;
+};
+
+const solution = countDepthIncreases(input);
+
+console.log(solution); // 1301
