9323c770468987547df2e971e23258e852b38146
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-12-04 10:30:43 +0100

    day04/part1 ✔️ SOLVED

--- day04/part1.ts
+++ day04/part1.ts
@@ -28,7 +28,7 @@ const parseInput = async (filePath: string) => {
   const text = await Deno.readTextFile(filePath);
 
   const inputParts = text.split(/\r\n/);
-  const incomingNumbers = inputParts[0];
+  const incomingNumbers = inputParts[0].split(",").map((str) => parseInt(str));
 
   const boardStream = inputParts.filter(
     (line, index) => index > 0 && line.length > 0
@@ -49,7 +49,72 @@ const parseInput = async (filePath: string) => {
   return { incomingNumbers, boards };
 };
 
-const { incomingNumbers, boards } = await parseInput("day04/testInput.txt");
+const hasRowWin = (board: Board): boolean => {
+  return board.some((row) => row.every((cell) => cell.marked));
+};
 
-console.log(incomingNumbers);
-console.log(boards);
+const hasColWin = (board: Board): boolean => {
+  for (let colIndex = 0; colIndex < board[0].length; colIndex++) {
+    const column = board.map((row) => row[colIndex]);
+    if (column.every((cell) => cell.marked)) return true;
+  }
+  return false;
+};
+
+const isWin = (board: Board): boolean => {
+  return hasRowWin(board) || hasColWin(board);
+};
+
+const applyNumberToRow = (row: BoardRow, numberToMark: number): BoardRow => {
+  const newRow = row.map(({ value, marked }) => ({
+    value,
+    marked: marked || numberToMark === value,
+  }));
+  return newRow as BoardRow;
+};
+
+const applyNumberToBoard = (board: Board, numberToMark: number): Board => {
+  const newBoard = board.map((row) => applyNumberToRow(row, numberToMark));
+  return newBoard as Board;
+};
+
+const applyNumberToBoards = (
+  boards: Array<Board>,
+  numberToMark: number
+): Array<Board> => {
+  return boards.map((board) => applyNumberToBoard(board, numberToMark));
+};
+
+/**
+ * @returns undefined if there are no winners yet
+ */
+const findWinner = (boards: Array<Board>): Board | undefined => {
+  return boards.find(isWin);
+};
+
+const getScore = (board: Board, lastCalledNumber: number): number => {
+  const unmarkedSum = board
+    .flatMap((row) => row)
+    .filter((cell) => !cell.marked)
+    .map((cell) => cell.value)
+    .reduce((acc, curr) => acc + curr, 0);
+  return unmarkedSum * lastCalledNumber;
+};
+
+/**
+ * @returns The final score of the winner
+ */
+const playUntilWinner = (
+  incomingNumbers: Array<number>,
+  boards: Array<Board>
+): number => {
+  const [numberToApply, ...remainingNumbers] = incomingNumbers;
+  const newBoards = applyNumberToBoards(boards, numberToApply);
+  const winner = findWinner(newBoards);
+  if (!winner) return playUntilWinner(remainingNumbers, newBoards);
+  return getScore(winner, numberToApply);
+};
+
+const { incomingNumbers, boards } = await parseInput("day04/input.txt");
+const winnerScore = playUntilWinner(incomingNumbers, boards);
+console.log(winnerScore); // 50008
