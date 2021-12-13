0d6e1f40c0b4b2173c173ed1e0a58d5fb869de54
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-12-13 09:09:27 +0100

    day13/part1 ✔️ SOLVED

--- day13/origami.ts
+++ day13/origami.ts
@@ -1,12 +1,48 @@
+import { getInputStrings } from "../utils/inputparsing.ts";
+
 export const parseInput = (inputLines: Array<string>) => {
   const nonEmptyStrs = inputLines.filter((str) => str.length > 0);
   const pointLines = nonEmptyStrs.filter((str) => !str.includes("fold"));
   const foldLines = nonEmptyStrs.filter((str) => str.includes("fold"));
   return {
-    points: pointLines.map((str) => str.split(",").map((n) => parseInt(n))),
+    points: pointLines.map(
+      (str) => str.split(",").map((n) => parseInt(n)) as [number, number]
+    ),
     folds: foldLines.map((str) => ({
-      type: str.includes("y") ? "y" : "x",
+      type: (str.includes("x") ? "x" : "y") as "x" | "y",
       value: parseInt(str.split("=")[1]),
     })),
   };
+};
+
+const foldPoint = (
+  [x, y]: [number, number],
+  { type, value }: { type: "x" | "y"; value: number }
+) => {
+  if (type === "x") {
+    if (x < value) return [x, y];
+    return [2 * value - x, y];
+  } else {
+    if (y < value) return [x, y];
+    return [x, 2 * value - y];
+  }
+};
+
+export const foldPoints = (
+  points: Array<[number, number]>,
+  foldInstruction: { type: "x" | "y"; value: number }
+) => {
+  const folded = points.map((point) => foldPoint(point, foldInstruction));
+  const uniquePointStrings = new Set(folded.map(([x, y]) => `${x},${y}`));
+  return [...uniquePointStrings.values()].map(
+    (pointString) =>
+      pointString.split(",").map((s) => parseInt(s)) as [number, number]
+  );
+};
+
+export const solvePart1 = (filePath: string) => {
+  const inputLines = getInputStrings(filePath);
+  const { points, folds } = parseInput(inputLines);
+  const foldedPoints = foldPoints(points, folds[0]);
+  return foldedPoints.length;
 };
--- day13/test.ts
+++ day13/test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import { parseInput } from "./origami.ts";
+import { parseInput, foldPoints, solvePart1 } from "./origami.ts";
 
 Deno.test("parses input", () => {
   assertEquals(
@@ -57,4 +57,47 @@ Deno.test("parses input", () => {
       ],
     }
   );
+});
+
+Deno.test("folds points", () => {
+  assertEquals(
+    foldPoints(
+      [
+        [1, 1],
+        [2, 7],
+        [2, 6],
+        [6, 6],
+        [7, 3],
+      ],
+      { type: "x", value: 4 }
+    ),
+    [
+      [1, 1],
+      [2, 7],
+      [2, 6],
+      [1, 3],
+    ]
+  );
+  assertEquals(
+    foldPoints(
+      [
+        [1, 1],
+        [2, 7],
+        [6, 6],
+        [9, 3],
+      ],
+      { type: "y", value: 5 }
+    ),
+    [
+      [1, 1],
+      [2, 3],
+      [6, 4],
+      [9, 3],
+    ]
+  );
+});
+
+Deno.test("solves part 1", () => {
+  assertEquals(solvePart1("day13/testinput.txt"), 17);
+  assertEquals(solvePart1("day13/input.txt"), 745);
 });
