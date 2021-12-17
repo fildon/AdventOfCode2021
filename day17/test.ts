09c48ffbb0866fd1e37027a75051c4c7b28bbf8c
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-12-17 09:15:26 +0100

    day17/part1 🪡 probe constructor and advancement

--- day17/test.ts
+++ day17/test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import { parseInput } from "./trickShot.ts";
+import { parseInput, buildProbe } from "./trickShot.ts";
 
 Deno.test("day17/parses input", () => {
   assertEquals(parseInput("day17/testinput.txt"), {
@@ -14,4 +14,21 @@ Deno.test("day17/parses input", () => {
     yMin: -85,
     yMax: -56,
   });
+});
+
+Deno.test("day17/advances probe", () => {
+  const probeA = buildProbe([0, 0], [7, 2]);
+  const { position: posA, velocity: velA } = probeA.advance();
+  assertEquals(posA, [7, 2]);
+  assertEquals(velA, [6, 1]);
+
+  const probeB = buildProbe([1, 3], [-7, -2]);
+  const { position: posB, velocity: velB } = probeB.advance();
+  assertEquals(posB, [-6, 1]);
+  assertEquals(velB, [-6, -3]);
+
+  const probeC = buildProbe([1, 3], [0, -2]);
+  const { position: posC, velocity: velC } = probeC.advance();
+  assertEquals(posC, [1, 1]);
+  assertEquals(velC, [0, -3]);
 });
--- day17/trickShot.ts
+++ day17/trickShot.ts
@@ -13,3 +13,32 @@ export const parseInput = (filePath: string) => {
     yMax: parseInt(matches[4]),
   };
 };
+
+type Vector = [number, number];
+
+type Probe = {
+  position: Vector;
+  velocity: Vector;
+  advance: () => Probe;
+};
+
+const add = ([aX, aY]: Vector, [bX, bY]: Vector): Vector => [aX + bX, aY + bY];
+
+const updateVelocity = ([dX, dY]: Vector): Vector => {
+  const drag =
+    [
+      [(v: number) => v > 0, -1] as const,
+      [(v: number) => v < 0, +1] as const,
+    ].find(([condition]) => condition(dX))?.[1] ?? 0;
+  return [dX + drag, dY - 1];
+};
+
+export const buildProbe = (position: Vector, velocity: Vector): Probe => {
+  const advance = () =>
+    buildProbe(add(position, velocity), updateVelocity(velocity));
+  return {
+    position,
+    velocity,
+    advance,
+  };
+};
