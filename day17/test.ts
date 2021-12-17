345c1b4722189c52c50137997895f888fd1d2a04
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-12-17 09:44:44 +0100

    day17/part1 🥨 inArea check

--- day17/test.ts
+++ day17/test.ts
@@ -1,34 +1,43 @@
-import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import { parseInput, buildProbe } from "./trickShot.ts";
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.117.0/testing/asserts.ts";
+import { parseInput, buildProbe, inArea } from "./trickShot.ts";
 
 Deno.test("day17/parses input", () => {
-  assertEquals(parseInput("day17/testinput.txt"), {
-    xMin: 20,
-    xMax: 30,
-    yMin: -10,
-    yMax: -5,
-  });
-  assertEquals(parseInput("day17/input.txt"), {
-    xMin: 248,
-    xMax: 285,
-    yMin: -85,
-    yMax: -56,
-  });
+  assertEquals(parseInput("day17/testinput.txt"), [20, 30, -10, -5]);
+  assertEquals(parseInput("day17/input.txt"), [248, 285, -85, -56]);
 });
 
 Deno.test("day17/advances probe", () => {
-  const probeA = buildProbe([0, 0], [7, 2]);
+  const probeA = buildProbe({ position: [0, 0], velocity: [7, 2] });
   const { position: posA, velocity: velA } = probeA.advance();
   assertEquals(posA, [7, 2]);
   assertEquals(velA, [6, 1]);
 
-  const probeB = buildProbe([1, 3], [-7, -2]);
+  const probeB = buildProbe({ position: [1, 3], velocity: [-7, -2] });
   const { position: posB, velocity: velB } = probeB.advance();
   assertEquals(posB, [-6, 1]);
   assertEquals(velB, [-6, -3]);
 
-  const probeC = buildProbe([1, 3], [0, -2]);
+  const probeC = buildProbe({ position: [1, 3], velocity: [0, -2] });
   const { position: posC, velocity: velC } = probeC.advance();
   assertEquals(posC, [1, 1]);
   assertEquals(velC, [0, -3]);
+});
+
+Deno.test("day17/is in target", () => {
+  // Positive cases
+  assert(inArea([1, 1], [0, 0, 2, 2]));
+  assert(inArea([0, 1], [0, 0, 2, 2]));
+  assert(inArea([0, 0], [0, 0, 2, 2]));
+  assert(inArea([0, 2], [0, 0, 2, 2]));
+  assert(inArea([2, 0], [0, 0, 2, 2]));
+  assert(inArea([2, 2], [0, 0, 2, 2]));
+
+  // Negative cases
+  assert(!inArea([3, 2], [0, 0, 2, 2]));
+  assert(!inArea([1, 3], [0, 0, 2, 2]));
+  assert(!inArea([9, 3], [0, 0, 2, 2]));
+  assert(!inArea([-1, 1], [0, 0, 2, 2]));
 });
--- day17/trickShot.ts
+++ day17/trickShot.ts
@@ -1,25 +1,33 @@
 import { getInputStrings } from "../utils/inputparsing.ts";
 
-export const parseInput = (filePath: string) => {
-  const inputLine = getInputStrings(filePath)[0];
-  const matches = inputLine.match(
-    /^target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)$/
-  );
-  if (!matches) throw new Error("Unrecognised input");
-  return {
-    xMin: parseInt(matches[1]),
-    xMax: parseInt(matches[2]),
-    yMin: parseInt(matches[3]),
-    yMax: parseInt(matches[4]),
-  };
-};
-
+/**
+ * 2D vector of the form: `[x, y]`
+ */
 type Vector = [number, number];
+
+/**
+ * Rectangular area bounded by `[xMin, yMin, xMax, yMax]`
+ */
+type Area = [number, number, number, number];
 
 type Probe = {
   position: Vector;
   velocity: Vector;
   advance: () => Probe;
+};
+
+export const parseInput = (filePath: string): Area => {
+  const inputLine = getInputStrings(filePath)[0];
+  const matches = inputLine.match(
+    /^target area: x=(-?\d+)..(-?\d+), y=(-?\d+)..(-?\d+)$/
+  );
+  if (!matches) throw new Error("Unrecognised input");
+  return [
+    parseInt(matches[1]),
+    parseInt(matches[2]),
+    parseInt(matches[3]),
+    parseInt(matches[4]),
+  ];
 };
 
 const add = ([aX, aY]: Vector, [bX, bY]: Vector): Vector => [aX + bX, aY + bY];
@@ -33,12 +41,24 @@ const updateVelocity = ([dX, dY]: Vector): Vector => {
   return [dX + drag, dY - 1];
 };
 
-export const buildProbe = (position: Vector, velocity: Vector): Probe => {
+export const buildProbe = ({
+  position = [0, 0],
+  velocity = [0, 0],
+}: {
+  position?: Vector;
+  velocity?: Vector;
+} = {}): Probe => {
   const advance = () =>
-    buildProbe(add(position, velocity), updateVelocity(velocity));
+    buildProbe({
+      position: add(position, velocity),
+      velocity: updateVelocity(velocity),
+    });
   return {
     position,
     velocity,
     advance,
   };
 };
+
+export const inArea = ([x, y]: Vector, [xMin, yMin, xMax, yMax]: Area) =>
+  xMin <= x && x <= xMax && yMin <= y && y <= yMax;
