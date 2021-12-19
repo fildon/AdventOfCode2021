c458cdd657e0afeeaeb3115fd64b3b543846bed7
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-12-19 13:30:10 +0100

    day19/part1 🥙 refactoring

--- day19/beacon.ts
+++ day19/beacon.ts
@@ -4,44 +4,6 @@
 type Vector = [number, number, number];
 type Scanner = Array<Vector>;
 
-export const getRotations = ([x, y, z]: Vector) => [
-  // x = 1
-  [x, y, z],
-  [x, -z, y],
-  [x, -y, -z],
-  [x, z, -y],
-
-  // y = 1
-  [z, x, y],
-  [-y, x, z],
-  [-z, x, -y],
-  [y, x, -z],
-
-  // z = 1
-  [y, z, x],
-  [-z, y, x],
-  [-y, -z, x],
-  [z, -y, x],
-
-  // x = -1
-  [-x, -z, -y],
-  [-x, y, -z],
-  [-x, z, y],
-  [-x, -y, z],
-
-  // y = -1
-  [-y, -x, -z],
-  [z, -x, -y],
-  [y, -x, z],
-  [-z, -x, y],
-
-  // z = -1
-  [-z, -y, -x],
-  [y, -z, -x],
-  [z, y, -x],
-  [-y, z, -x],
-];
-
 const equals = ([a, b, c]: Vector) =>
   ([x, y, z]: Vector) => a === x && b === y && c === z;
 
@@ -58,6 +20,9 @@ export const countOverlaps = (
   scannerB: Scanner,
 ) => count(scannerA, inCollection(scannerB));
 
+/**
+ * Given a scanner returns its 24 rotational equivalent forms
+ */
 const rotations = (scanner: Scanner): Array<Scanner> => [
   scanner,
   scanner.map(([x, y, z]) => [x, -z, y]),
@@ -92,19 +57,17 @@ const negate = ([a, b, c]: Vector): Vector => [-a, -b, -c];
 
 const minus = (v: Vector) => add(negate(v));
 
+const createOffsets = (scanner: Scanner) =>
+  (vector: Vector) => scanner.map(minus(vector));
+
 const applyOffset = (scanner: Scanner) =>
   (offSet: Vector) => scanner.map(add(offSet));
 
 const offsets = (scannerA: Scanner) =>
-  (scannerB: Scanner): Array<Scanner> => {
-    const offSets: Array<Vector> = [];
-    for (const a of scannerA) {
-      for (const b of scannerB) {
-        offSets.push(minus(b)(a));
-      }
-    }
-    return offSets.map(applyOffset(scannerB));
-  };
+  (scannerB: Scanner): Array<Scanner> =>
+    scannerB.flatMap(createOffsets(scannerA)).map(
+      applyOffset(scannerB),
+    );
 
 const overlaps = (scannerA: Scanner) =>
   (scannerB: Scanner) => countOverlaps(scannerA, scannerB) >= 12;
--- day19/test.ts
+++ day19/test.ts
@@ -1,45 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import { countOverlaps, getRotations, lockPair } from "./beacon.ts";
-
-Deno.test("day19/get rotations", () => {
-  assertEquals(getRotations([1, 2, 3]), [
-    // x = 1
-    [1, 2, 3],
-    [1, -3, 2],
-    [1, -2, -3],
-    [1, 3, -2],
-
-    // y = 1
-    [3, 1, 2],
-    [-2, 1, 3],
-    [-3, 1, -2],
-    [2, 1, -3],
-
-    // z = 1
-    [2, 3, 1],
-    [-3, 2, 1],
-    [-2, -3, 1],
-    [3, -2, 1],
-
-    // x = -1
-    [-1, -3, -2],
-    [-1, 2, -3],
-    [-1, 3, 2],
-    [-1, -2, 3],
-
-    // y = -1
-    [-2, -1, -3],
-    [3, -1, -2],
-    [2, -1, 3],
-    [-3, -1, 2],
-
-    // z = -1
-    [-3, -2, -1],
-    [2, -3, -1],
-    [3, 2, -1],
-    [-2, 3, -1],
-  ]);
-});
+import { countOverlaps, lockPair } from "./beacon.ts";
 
 Deno.test("day19/count overlaps", () => {
   assertEquals(countOverlaps([], []), 0);
