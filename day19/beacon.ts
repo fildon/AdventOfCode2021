6bfb56b3c33d5891aebdee0852f0b8104fe6adf4
Rupert Foggo McKay <maxfmckay@gmail.com>
2021-12-19 13:19:28 +0100

    day19/part1 🍜 locking

--- day19/beacon.ts
+++ day19/beacon.ts
@@ -2,6 +2,7 @@
  * 3D vector of the form [x, y, z]
  */
 type Vector = [number, number, number];
+type Scanner = Array<Vector>;
 
 export const getRotations = ([x, y, z]: Vector) => [
   // x = 1
@@ -53,6 +54,69 @@ const count = <T>(
 ) => collection.filter(condition).length;
 
 export const countOverlaps = (
-  vectorsA: Array<Vector>,
-  vectorsB: Array<Vector>,
-) => count(vectorsA, inCollection(vectorsB));
+  scannerA: Scanner,
+  scannerB: Scanner,
+) => count(scannerA, inCollection(scannerB));
+
+const rotations = (scanner: Scanner): Array<Scanner> => [
+  scanner,
+  scanner.map(([x, y, z]) => [x, -z, y]),
+  scanner.map(([x, y, z]) => [x, -y, -z]),
+  scanner.map(([x, y, z]) => [x, z, -y]),
+  scanner.map(([x, y, z]) => [z, x, y]),
+  scanner.map(([x, y, z]) => [-y, x, z]),
+  scanner.map(([x, y, z]) => [-z, x, -y]),
+  scanner.map(([x, y, z]) => [y, x, -z]),
+  scanner.map(([x, y, z]) => [y, z, x]),
+  scanner.map(([x, y, z]) => [-z, y, x]),
+  scanner.map(([x, y, z]) => [-y, -z, x]),
+  scanner.map(([x, y, z]) => [z, -y, x]),
+  scanner.map(([x, y, z]) => [-x, -z, -y]),
+  scanner.map(([x, y, z]) => [-x, y, -z]),
+  scanner.map(([x, y, z]) => [-x, z, y]),
+  scanner.map(([x, y, z]) => [-x, -y, z]),
+  scanner.map(([x, y, z]) => [-y, -x, -z]),
+  scanner.map(([x, y, z]) => [z, -x, -y]),
+  scanner.map(([x, y, z]) => [y, -x, z]),
+  scanner.map(([x, y, z]) => [-z, -x, y]),
+  scanner.map(([x, y, z]) => [-z, -y, -x]),
+  scanner.map(([x, y, z]) => [y, -z, -x]),
+  scanner.map(([x, y, z]) => [z, y, -x]),
+  scanner.map(([x, y, z]) => [-y, z, -x]),
+];
+
+const add = ([a, b, c]: Vector) =>
+  ([x, y, z]: Vector): Vector => [a + x, b + y, c + z];
+
+const negate = ([a, b, c]: Vector): Vector => [-a, -b, -c];
+
+const minus = (v: Vector) => add(negate(v));
+
+const applyOffset = (scanner: Scanner) =>
+  (offSet: Vector) => scanner.map(add(offSet));
+
+const offsets = (scannerA: Scanner) =>
+  (scannerB: Scanner): Array<Scanner> => {
+    const offSets: Array<Vector> = [];
+    for (const a of scannerA) {
+      for (const b of scannerB) {
+        offSets.push(minus(b)(a));
+      }
+    }
+    return offSets.map(applyOffset(scannerB));
+  };
+
+const overlaps = (scannerA: Scanner) =>
+  (scannerB: Scanner) => countOverlaps(scannerA, scannerB) >= 12;
+
+/**
+ * Attempts to lock scannerB relative to scannerA
+ *
+ * Returns scannerB's probes in scannerA's reference frame if a lock can be
+ * found, otherwise returns undefined
+ */
+export const lockPair = (
+  scannerA: Array<Vector>,
+  scannerB: Array<Vector>,
+): Array<Vector> | undefined =>
+  rotations(scannerB).flatMap(offsets(scannerA)).find(overlaps(scannerA));
