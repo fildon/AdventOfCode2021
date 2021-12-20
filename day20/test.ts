802982251a8b6423fa11c8c8d70d785033f8077b
Rupert Foggo McKay <rupert.mckay@adidas.com>
2021-12-20 11:22:59 +0100

    day20/part1 🐢 initial input parsing

--- day20/test.ts
+++ day20/test.ts
@@ -1,6 +1,8 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import { foo } from "./trench.ts";
+import { parseInput } from "./trench.ts";
 
-Deno.test("foo test", () => {
-  assertEquals(foo(), 1);
+Deno.test("day20/parses input", () => {
+  const { imageEnhancement, image } = parseInput("day20/testinput.txt");
+  assertEquals(imageEnhancement.slice(0, 30), "..#.#..#####.#.#.#.###.##.....");
+  assertEquals(image[2], "##..#");
 });
--- day20/trench.ts
+++ day20/trench.ts
@@ -1,1 +1,8 @@
-export const foo = () => 1;
+import { getInputStrings } from "../utils/inputparsing.ts";
+
+export const parseInput = (filePath: string) => {
+  const inputStrings = getInputStrings(filePath);
+  const imageEnhancement = inputStrings[0];
+  const image = inputStrings.slice(1).filter((str) => str.length > 0);
+  return { imageEnhancement, image };
+};
