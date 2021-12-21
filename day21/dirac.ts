a66b373d41ad57fb81c934f0d2fe54578dfe0b7d
Rupert Foggo McKay <rupert.mckay@adidas.com>
2021-12-21 13:18:29 +0100

    day21/part1 🍳 configurable mod numbers

--- day21/dirac.ts
+++ day21/dirac.ts
@@ -1,10 +1,31 @@
+/**
+ * Simulates modulo numbers, but with a configurable min and max
+ *
+ * min-max range is inclusive at both ends
+ */
+export const buildCircularRange = (
+  { min = 1, max }: { min?: number; max: number },
+) =>
+  (value: number) => {
+    let result = value;
+    const rangeSize = max - min + 1;
+    while (result < min) {
+      result += rangeSize;
+    }
+    while (result > max) {
+      result -= rangeSize;
+    }
+    return result;
+  };
+
 export const buildDie = () => {
+  const range1To100 = buildCircularRange({ min: 1, max: 100 });
   let currentValue = 0;
   let rollCounter = 0;
 
   const roll = () => {
     rollCounter++;
-    currentValue = (currentValue % 100) + 1;
+    currentValue = range1To100(currentValue + 1);
     return currentValue;
   };
 
@@ -12,4 +33,8 @@ export const buildDie = () => {
     roll,
     getCurrentRollCount: () => rollCounter,
   };
+};
+
+export const solvePart1 = (p1Start: number, p2Start: number) => {
+  return 0;
 };
--- day21/test.ts
+++ day21/test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import { buildDie } from "./dirac.ts";
+import { buildCircularRange, buildDie, solvePart1 } from "./dirac.ts";
 
 Deno.test("day21/build die", () => {
   const die = buildDie();
@@ -27,4 +27,25 @@ Deno.test("day21/die wraps", () => {
   assertEquals(hundredthRoll, 100);
   assertEquals(wrappedRoll, 1);
   assertEquals(die.getCurrentRollCount(), 101);
+});
+
+Deno.test("day21/build circular range", () => {
+  const range1To10 = buildCircularRange({ min: 1, max: 10 });
+  assertEquals(range1To10(1), 1);
+  assertEquals(range1To10(5), 5);
+  assertEquals(range1To10(10), 10);
+  assertEquals(range1To10(11), 1);
+  assertEquals(range1To10(100), 10);
+  assertEquals(range1To10(-1), 9);
+});
+
+Deno.test({
+  name: "day21/solves part 1",
+  ignore: true,
+  fn: () => {
+    // test input
+    assertEquals(solvePart1(4, 8), 739785);
+    // real input
+    // assertEquals(solvePart1(3, 10), -1);
+  },
 });
