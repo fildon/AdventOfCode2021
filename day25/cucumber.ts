8ff76c6fcacdb4cd5476b6cd3f8d957757805110
Rupert Foggo McKay <rupert.mckay@adidas.com>
2021-12-25 13:57:48 +0100

    day25/part1 🥓 step function

--- day25/cucumber.ts
+++ day25/cucumber.ts
@@ -6,7 +6,40 @@ export const equals = (a: Cucumbers, b: Cucumbers): boolean =>
   a.length === b.length && a.every((str, index) => str === b[index]);
 
 export const step = (current: Cucumbers): Cucumbers => {
-  throw new Error("TODO");
+  const height = current.length;
+  const afterEastMoves = current.map((line) => {
+    const { movers, neighbours } = [...line].map((
+      _,
+      i,
+    ) => [i, (i + 1) % line.length])
+      .filter(([mover, neighbour]) =>
+        line[mover] === ">" && line[neighbour] === "."
+      ).reduce((acc, [mover, neighbour]) => {
+        acc.movers.add(mover);
+        acc.neighbours.add(neighbour);
+        return acc;
+      }, {
+        movers: new Set<number>(),
+        neighbours: new Set<number>(),
+      });
+    return [...line].map((cell, i) =>
+      movers.has(i) ? "." : neighbours.has(i) ? ">" : cell
+    ).join("");
+  });
+
+  const afterSouthMoves = afterEastMoves.map((line, y) =>
+    [...line].map((cell, x) => {
+      if (cell === ">") return ">";
+      if (cell === "v") {
+        if (afterEastMoves[(y + 1) % height][x] === ".") return ".";
+        return "v";
+      }
+      // cell === "."
+      if (afterEastMoves[(y + height - 1) % height][x] === "v") return "v";
+      return ".";
+    }).join("")
+  );
+  return afterSouthMoves;
 };
 
 /**
--- day25/test.ts
+++ day25/test.ts
@@ -1,9 +1,56 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import { equals } from "./cucumber.ts";
+import { equals, step } from "./cucumber.ts";
 
 Deno.test("day25/equals", () => {
   assertEquals(equals([], []), true);
   assertEquals(equals([], ["a"]), false);
   assertEquals(equals(["a"], ["b"]), false);
   assertEquals(equals(["b"], ["b"]), true);
+});
+
+Deno.test("day25/step", () => {
+  assertEquals(step([]), []);
+  assertEquals(step(["."]), ["."]);
+  assertEquals(step([">."]), [".>"]);
+  assertEquals(step([".>>."]), [".>.>"]);
+  assertEquals(step([".>>.", "...>"]), [".>.>", ">..."]);
+  assertEquals(step(["v", "."]), [".", "v"]);
+  assertEquals(
+    step([
+      "..........",
+      ".>v....v..",
+      ".......>..",
+      "..........",
+    ]),
+    [
+      "..........",
+      ".>........",
+      "..v....v>.",
+      "..........",
+    ],
+  );
+  assertEquals(
+    step([
+      "v...>>.vv>",
+      ".vv>>.vv..",
+      ">>.>v>...v",
+      ">>v>>.>.v.",
+      "v>v.vv.v..",
+      ">.>>..v...",
+      ".vv..>.>v.",
+      "v.v..>>v.v",
+      "....v..v.>",
+    ]),
+    [
+      "....>.>v.>",
+      "v.v>.>v.v.",
+      ">v>>..>v..",
+      ">>v>v>.>.v",
+      ".>v.v...v.",
+      "v>>.>vvv..",
+      "..v...>>..",
+      "vv...>>vv.",
+      ">.v.v..v.v",
+    ],
+  );
 });
